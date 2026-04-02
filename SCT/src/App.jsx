f133071ffeb4b315bc963ea4fd2bdf9b26ee7405
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-04-02 22:10:36 +0530

    pint

--- SCT/src/App.jsx
+++ SCT/src/App.jsx
@@ -5,21 +5,32 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 function App() {
   const container = useRef(null);
-
+  
   useGSAP(
     () => {
+      const boxes = gsap.utils.toArray(".box");
       const tl = gsap.timeline();
-      tl.to(".b", {
+      tl.to(".parent", {
         scrollTrigger: {
-          markers:true,
-          trigger: ".b",
-          start: "top 60%",
-          end: "top 20%",
-          scrub: 2 ,
+          trigger: ".parent",
+          start: "center 40%",
+          end: "center 20%",
+          scrub:2,
+          pin: true,
         },
-        x: 500,
-        opacity: 1,
-        
+      });
+      boxes.forEach((box) => {
+        tl.to(box, {
+          scrollTrigger: {
+            markers: true,
+            trigger: box,
+            start: "top 40%",
+            end: "center 20%",
+            scrub: 2,
+          },
+          x: 500,
+          opacity: 1,
+        });
       });
     },
     { scope: container },
@@ -27,8 +38,8 @@ function App() {
 
   return (
     <>
-      <div ref={container}>
-        <div className="h-[300vh] p-30 flex flex-col gap-20">
+      <div ref={container} className="h-[300vh]">
+        <div className="h-[50vh] parent mt-60 p-30 flex bg-mist-700 gap-2">
           <div className="a h-40 box w-30 bg-cyan-800"> </div>
           <div className="b h-40 box w-30 bg-red-800"></div>
           <div className="c h-40 box w-30 bg-blue-800"></div>
